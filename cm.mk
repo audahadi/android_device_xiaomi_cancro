297e22dcc6efd31c8a979f7cd9d0b71332b91e47
MrBaNkS <banksmi09@gmail.com>
2016-01-27 23:40:11 +0800

    Kang audio config from Xiaomi-dev * with help from https://github.com/rooque/omni_device_cancro/tree/android-6.0 * credits rooque and Xiaomi * also update from Xiaomi cancro MM image * credits to Bitti09 for the missing kernel headers

--- cm.mk
+++ cm.mk
@@ -14,17 +14,17 @@
 
 TARGET_CONTINUOUS_SPLASH_ENABLED := true
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
 # Inherit from cancro device
 $(call inherit-product, device/xiaomi/cancro/cancro.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common CM stuff (mini)
 $(call inherit-product, vendor/cm/config/common_mini_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 # Product configuration
 PRODUCT_NAME := cm_cancro
@@ -42,6 +42,3 @@ TARGET_VENDOR_DEVICE_NAME := MI3W
 TARGET_UNOFFICIAL_BUILD_ID := audahadi
 
 PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=cancro PRODUCT_NAME=cancro
-
-## Use the latest approved GMS identifiers unless running a signed build
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Xiaomi/cancro/cancro:6.0/MDB08M/5.11.6:userdebug/test-keys PRIVATE_BUILD_DESC="cancro-userdebug 6.0 MDB08M 5.11.6 test-keys"
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,6 @@
 add_lunch_combo cm_cancro-user
 add_lunch_combo cm_cancro-userdebug
 add_lunch_combo cm_cancro-eng
+
+# Run device patches on the tree
+sh device/xiaomi/cancro/patches/apply.sh
